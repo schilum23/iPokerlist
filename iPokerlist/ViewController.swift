a140664fec75083ee2b3da159658576e4dc1273f
Oliver Rosner <Schilum23@Olivers-MacBook-Pro.local>
2015-03-08 16:00:37 +0100

    Initial Commit

---
+++ iPokerlist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iPokerlist
+//
+//  Created by Oliver Rosner on 08.03.15.
+//  Copyright (c) 2015 Oliver Rosner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
